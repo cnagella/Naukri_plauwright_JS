99b471af7485dd0ab806c5ff52c522496e85d461
Venkat272002 <pattemvenkatasurendra@gmail.com>
2025-09-18 17:53:48 +0530

    added creds through cred,json file

--- tests/homePage.spec.js
+++ tests/homePage.spec.js
@@ -1,15 +1,13 @@
 const {test, expect}= require('@playwright/test');
 const {LoginPage} = require('../pages/login.page');
 const {HomePage}= require("../pages/homePage.page");
-
-const username = process.env.USERNAME;
-const password = process.env.PASSWORD;
+const testData = JSON.parse(JSON.stringify(require('../utils/creds.json')));
 
 
 test('Verify user is navigating to the recommended jobs tab', async ({page})=>{
     const loginPage = new LoginPage(page);
     await loginPage.gotoLoginPage();
-    await loginPage.login(username, password);
+    await loginPage.login(testData.username, testData.password);
     const homePage = new HomePage(page);
     await homePage.gotoearlyAccessRolesTab();
     await homePage.shareInterestToAllEarlyAccessJobs();
